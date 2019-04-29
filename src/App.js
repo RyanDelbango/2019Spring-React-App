be62253aaf802354a5a589954bcddb4cdfb9d929
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-04-29 09:54:49 -0400

    clean build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Nav from './components/Nav';
 
-const Home = ()=> <h1>You are Home!</h1>
+const Home = ()=> <h1>You are, yes you are home!!</h1>
 const About = ()=> <h1>I teach the best Web Course at New Paltz!</h1>
 const Friends = ()=> (<ul>
   <li>You guys</li>
