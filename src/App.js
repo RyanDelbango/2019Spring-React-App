c6901a3d5b3558039565ba973b7675bedb2ebad5
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-05-01 22:12:45 -0400

    props instead of globals

--- src/App.js
+++ src/App.js
@@ -11,15 +11,22 @@ import Login from './views/Login';
 const Home = ()=> <h1>You are, yes you are home!!</h1>
 
 function App() {
+  const [user, setUser ] = React.useState(null);
+  const [oAuthUser, setoAuthUser] = React.useState(null);
+  //{id:"", FirstName:"", LastName:""}
+  function login(m){
+    setoAuthUser(m.response2 || { });
+    setUser(m.x.user);
+  }
+
   return (
     <div className="App">
       <Router>
-        <Nav />
-        
+      <Nav user={user} oAuthUser={oAuthUser} />        
         <Route path="/" component={Home} />
         <Route path="/About" component={About} />
-        <Route path="/MyFriends" component={Friends} />
-        <Route path="/Login" component={Login} />
+        <Route path="/MyFriends" render={()=> <Friends user={user} />}  />
+        <Route path="/Login"  render={props=> <Login {...props} user={user} login={login} />}  />
       </Router>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
-// import { Globals } from "@/models/api";
-const Globals = { user: null, oAuthUser: null };
 
-export default ()=> (
+export default props=> (
      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <img src={logo} className="App-logo" alt="logo" height="30" />
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,16 +20,16 @@ export default ()=> (
                         <Link className="nav-link" to="/MyFriends" activeClassName="active">My Friends</Link>
                     </li>
                 </ul>
-                { !Globals.user &&
+                {!props.user &&
                 <form className="form-inline mt-2 mt-md-0">
                     <Link to="/Login" className="nav-link">Login</Link>
                     <Link to="/Register" className="nav-link">Sign Up</Link>
                 </form>
                 }
-                { Globals.user &&
+            {props.user &&
             <span className="navbar-text">
-                <img width="30" src={`https://graph.facebook.com/${Globals.oAuthUser.id}/picture`} alt="facebook.img" />
-                Welcome {Globals.user.firstName} {Globals.user.lastName}
+            <img width="30" src={`https://graph.facebook.com/${props.oAuthUser.id}/picture`} alt="facebook img"/>
+            Welcome {props.user.firstName} {props.user.lastName}
             </span>
                 }
             </div>
